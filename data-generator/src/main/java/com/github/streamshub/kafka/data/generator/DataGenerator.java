fc97baf8d697baa80be082fdae3a764309af1e13
Katherine Stanley <11195226+katheris@users.noreply.github.com>
2024-07-31 12:00:42 +0100

    Refactor data generator to run a single Kafka producer
    Signed-off-by: Katherine Stanley <11195226+katheris@users.noreply.github.com>

--- data-generator/src/main/java/com/github/streamshub/kafka/data/generator/DataGenerator.java
+++ data-generator/src/main/java/com/github/streamshub/kafka/data/generator/DataGenerator.java
@@ -6,17 +6,18 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
 
 public class DataGenerator implements Runnable {
     final String bootstrapServers;
-    final Data data;
+    final List<Data> dataTypes;
 
-    public DataGenerator(String bootstrapServers, Data data) {
+    public DataGenerator(String bootstrapServers, List<Data> dataTypes) {
         this.bootstrapServers = bootstrapServers;
-        this.data = data;
+        this.dataTypes = dataTypes;
     }
 
     @Override
@@ -24,34 +25,42 @@ public class DataGenerator implements Runnable {
         if (Boolean.parseBoolean(System.getenv("USE_APICURIO_REGISTRY"))) {
             String registryUrl = System.getenv("REGISTRY_URL");
             Producer<String, Object> producer = new KafkaProducer<>(KafkaClientProps.avro(bootstrapServers, registryUrl));
-            send(producer, data.topic(), () -> {
-                Map<String, String> generatedData = data.generate();
-                GenericRecord avroRecord = new GenericData.Record(data.schema());
-                generatedData.forEach(avroRecord::put);
-                return avroRecord;
-            });
+            send(producer, () -> dataTypes.stream().map(this::generateAvroRecord).toList());
         } else {
             Producer<String, String> producer = new KafkaProducer<>(KafkaClientProps.csv(bootstrapServers));
-            send(producer, data.topic(), () -> String.join(",", data.generate().values()));
+            send(producer, () -> dataTypes.stream().map(this::generateCsvRecord).toList());
         }
     }
 
-    private <V> void send(Producer<String, V> producer, String topic, Supplier<V> valueSupplier) {
+    private ProducerRecord<String, Object> generateAvroRecord(Data data) {
+        Map<String, String> generatedData = data.generate();
+        GenericRecord avroRecord = new GenericData.Record(data.schema());
+        generatedData.forEach(avroRecord::put);
+        return getKafkaProducerRecord(data, avroRecord);
+    }
+
+    private ProducerRecord<String, String> generateCsvRecord(Data data) {
+        return getKafkaProducerRecord(data, String.join(",", data.generate().values()));
+    }
+
+    private static <V> ProducerRecord<String, V> getKafkaProducerRecord(Data data, V value) {
+        String key = String.valueOf(System.currentTimeMillis());
+        return new ProducerRecord<>(
+                data.topic(),
+                key,
+                value);
+    }
+
+    private <V> void send(Producer<String, V> producer, Supplier<List<ProducerRecord<String, V>>> recordsSupplier) {
         try (producer) {
 
             while (true) {
-                String key = String.valueOf(System.currentTimeMillis());
-                V value = valueSupplier.get();
-                ProducerRecord<String, V> record =
-                        new ProducerRecord<>(
-                                topic,
-                                key,
-                                valueSupplier.get());
-
-                producer.send(record).get();
-
-                System.out.println("Kafka Data Generator : Sent record to topic " + topic + " with value: " +
-                        value);
+                List<ProducerRecord<String, V>> producerRecords = recordsSupplier.get();
+                for (ProducerRecord<String, V> record : producerRecords) {
+                    producer.send(record).get();
+                    System.out.println("Kafka Data Generator : Sent record to topic " + record.topic() + " with value: " +
+                            record.value());
+                }
 
                 Thread.sleep(3000);
 
--- data-generator/src/main/java/com/github/streamshub/kafka/data/generator/Main.java
+++ data-generator/src/main/java/com/github/streamshub/kafka/data/generator/Main.java
@@ -4,25 +4,16 @@ import com.github.streamshub.kafka.data.generator.examples.ClickStreamData;
 import com.github.streamshub.kafka.data.generator.examples.SalesData;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String bootstrapServers = System.getenv("KAFKA_BOOTSTRAP_SERVERS");
-        String[] dataTypes = System.getenv("DATA").split(",");
-        Arrays.stream(dataTypes)
+        List<Data> dataTypes = Arrays.stream(System.getenv("DATA").split(","))
                 .map(Main::getDataClass)
-                .forEach(data -> {
-                    System.out.println("Starting Data Generator " + data.getClass().getName() + "...");
-                    Thread dgThread = new Thread(new DataGenerator(bootstrapServers, data));
-                    dgThread.start();
-
-                    //So two classes don't fight when registering the schemas in the registry
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+                .toList();
+        Thread dgThread = new Thread(new DataGenerator(bootstrapServers, dataTypes));
+        dgThread.start();
     }
 
     private static Data getDataClass(String dataType) {
