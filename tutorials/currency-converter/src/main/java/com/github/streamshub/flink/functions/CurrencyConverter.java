72d5457c8c2a09bce670093814f038f99ca805b3
Piotr Płaczek <piotrpdev@gmail.com>
2025-06-18 11:37:00 +0100

    Combine `enum` and `Map` approaches

--- tutorials/currency-converter/src/main/java/com/github/streamshub/flink/functions/CurrencyConverter.java
+++ tutorials/currency-converter/src/main/java/com/github/streamshub/flink/functions/CurrencyConverter.java
@@ -1,25 +1,37 @@
 package com.github.streamshub.flink.functions;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.apache.flink.table.functions.ScalarFunction;
 
 public class CurrencyConverter extends ScalarFunction {
    // https://www.unicode.org/charts/nameslist/n_20A0.html
    // https://www.iso.org/iso-4217-currency-codes.html
    public enum Currency {
-      €("EUR"),
-      ₹("INR"),
-      ₺("TRY"),
-      ฿("THB"),
-      ₴("UAH"),
-      ₮("MNT"),
-      ERR("ERR");
+      EUR("€", "EUR"),
+      INR("₹", "INR"),
+      TRY("₺", "TRY"),
+      THB("฿", "THB"),
+      UAH("₴", "UAH"),
+      MNT("₮", "MNT"),
+      ERR("?", "ERR");
 
       public static final String SEPARATOR = " ";
+      private static final Map<String, Currency> SYMBOL_TO_CURRENCY = Stream.of(Currency.values())
+            .collect(Collectors.toMap(Currency::getSymbol, c -> c));
 
+      private final String symbol;
       private final String isoCode;
 
-      Currency(String isoCode) {
+      Currency(String symbol, String isoCode) {
+         this.symbol = symbol;
          this.isoCode = isoCode;
+      }
+
+      public String getSymbol() {
+         return symbol;
       }
 
       public String getIsoCode() {
@@ -29,9 +41,9 @@ public class CurrencyConverter extends ScalarFunction {
       public static Currency fromCurrencyAmount(String currencyAmount) {
          String currencySymbol = currencyAmount.substring(0, 1); // e.g. '€'
          try {
-            return Currency.valueOf(currencySymbol); // e.g. "€" => "EUR"
+            return SYMBOL_TO_CURRENCY.getOrDefault(currencySymbol, ERR);
          } catch (Exception e) {
-            return Currency.ERR; // e.g. ">" => "ERR"
+            return ERR;
          }
       }
 
@@ -46,6 +58,6 @@ public class CurrencyConverter extends ScalarFunction {
 
       String amount = currencyAmount.substring(1); // e.g. "100"
 
-      return currency.concatToAmount(amount);  // e.g. "100 EUR"
+      return currency.concatToAmount(amount); // e.g. "100 EUR"
    }
 }
