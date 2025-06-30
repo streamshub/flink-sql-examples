83ab98b382450773ae67a2fb4a2c5e9353569d12
Piotr Płaczek <piotrpdev@gmail.com>
2025-06-30 16:48:42 +0100

    Create topics with retention

--- tutorials/data-generator/src/main/java/com/github/streamshub/kafka/data/generator/DataGenerator.java
+++ tutorials/data-generator/src/main/java/com/github/streamshub/kafka/data/generator/DataGenerator.java
@@ -1,23 +1,38 @@
 package com.github.streamshub.kafka.data.generator;
 
+import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-import java.util.List;
+import java.util.*;
 import java.util.function.Supplier;
 
+import static org.apache.kafka.clients.CommonClientConfigs.*;
+import static org.apache.kafka.common.config.TopicConfig.RETENTION_MS_CONFIG;
+
 public class DataGenerator implements Runnable {
+    final static Map<String, String> RETENTION_CONFIG = Collections.singletonMap(RETENTION_MS_CONFIG, String.valueOf(60 * 60 * 1000)); // 1 hour
+    final static int KAFKA_ADMIN_CLIENT_REQUEST_TIMEOUT_MS_CONFIG = 5000;
+    final static String KAFKA_ADMIN_CLIENT_ID_CONFIG = "data-generator-admin-client";
     final String bootstrapServers;
     final List<Data> dataTypes;
+    final Properties kafkaAdminProps;
 
     public DataGenerator(String bootstrapServers, List<Data> dataTypes) {
         this.bootstrapServers = bootstrapServers;
         this.dataTypes = dataTypes;
+
+        kafkaAdminProps = new Properties();
+        kafkaAdminProps.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        kafkaAdminProps.put(CLIENT_ID_CONFIG, KAFKA_ADMIN_CLIENT_ID_CONFIG);
+        kafkaAdminProps.put(REQUEST_TIMEOUT_MS_CONFIG, String.valueOf(KAFKA_ADMIN_CLIENT_REQUEST_TIMEOUT_MS_CONFIG));
     }
 
     @Override
     public void run() {
+        createTopics(dataTypes.stream().map(Data::topic).toList());
+
         if (Boolean.parseBoolean(System.getenv("USE_APICURIO_REGISTRY"))) {
             String registryUrl = System.getenv("REGISTRY_URL");
             Producer<String, Object> producer = new KafkaProducer<>(KafkaClientProps.avro(bootstrapServers, registryUrl));
@@ -25,6 +40,28 @@ public class DataGenerator implements Runnable {
         } else {
             Producer<String, String> producer = new KafkaProducer<>(KafkaClientProps.csv(bootstrapServers));
             send(producer, () -> dataTypes.stream().map(this::generateCsvRecord).toList());
+        }
+    }
+
+    private void createTopics(List<String> topicNames) {
+        try (AdminClient adminClient = AdminClient.create(kafkaAdminProps)) {
+            Set<String> existingTopicNames = adminClient.listTopics().names().get();
+            List<NewTopic> newTopics = topicNames.stream()
+                    .filter(topicName -> !existingTopicNames.contains(topicName)) // createTopics() will throw if topic already exists
+                    .map(
+                    topicName -> new NewTopic(topicName, Optional.empty(), Optional.empty()).configs(RETENTION_CONFIG)
+                    )
+                    .toList();
+
+            if (newTopics.isEmpty()) {
+                return;
+            }
+
+            adminClient.createTopics(newTopics).all().get();
+            System.out.println("Kafka Data Generator : Successfully created topics: " + newTopics);
+        } catch (Exception e) {
+            System.out.println("Kafka Data Generator : Failed to create topics.");
+            throw new RuntimeException(e);
         }
     }
 
