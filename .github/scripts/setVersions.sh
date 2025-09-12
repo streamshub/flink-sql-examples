2326cfc806b248ef85548bd0ab81958c970d54ab
Piotr Płaczek <piotrpdev@gmail.com>
2025-09-12 09:43:11 +0100

    Update `setVersion.sh` and `release-transition.yaml` (#76)

--- .github/scripts/setVersions.sh
+++ .github/scripts/setVersions.sh
@@ -7,4 +7,4 @@ if [[ $# != 2 ]]; then
 fi
 mvn -B versions:set -DgenerateBackupPoms=false -DnewVersion=${1}
 sed --in-place --regexp-extended "s|flink-examples-data-generator:([^[:space:]]*)|flink-examples-data-generator:${2}|g" tutorials/recommendation-app/data-generator.yaml
-sed --in-place --regexp-extended "s|flink-examples-data-generator:([^[:space:]]*)|flink-examples-data-generator:${2}|g" tutorials/interactive-etl/data-generator.yaml
+sed --in-place --regexp-extended "s|flink-sql-runner-with-flink-udf-currency-converter:([^[:space:]]*)|flink-sql-runner-with-flink-udf-currency-converter:${2}|g" tutorials/user-defined-functions/standalone-etl-udf-deployment.yaml
