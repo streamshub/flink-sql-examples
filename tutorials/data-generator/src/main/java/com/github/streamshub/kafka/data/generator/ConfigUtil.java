d9137de950576f222cfb33f8a3ee6343c74a42a0
Piotr Płaczek <piotrpdev@gmail.com>
2025-09-24 14:07:41 +0100

    Get Kafka env vars using Strimzi approach in data-generator (#79)

---
+++ tutorials/data-generator/src/main/java/com/github/streamshub/kafka/data/generator/ConfigUtil.java
@@ -0,0 +1,42 @@
+// Copied from https://github.com/strimzi/client-examples/blob/main/java/common/src/main/java/io/strimzi/common/ConfigUtil.java
+package com.github.streamshub.kafka.data.generator;
+
+import java.util.Locale;
+import java.util.Properties;
+import java.util.stream.Collectors;
+import java.util.Map;
+
+
+/**
+ * Provides utility methods for managing common configuration properties.
+ */
+public class ConfigUtil {
+    private static final String KAFKA_PREFIX = "KAFKA_";
+
+    /**
+     * Converts environment variables into a corresponding property key format.
+     *
+     * @param envVar Name of the environment variable to be converted to property key format.
+     * @return Returns a String which removes a prefix containing '_', converts to lower case and replaces '_' with '.'.
+     */
+    public static String convertEnvVarToPropertyKey(String envVar) {
+        return envVar.substring(envVar.indexOf("_") + 1).toLowerCase(Locale.ENGLISH).replace("_", ".");
+    }
+
+    /**
+     * Retrieves Kafka-related properties from environment variables.
+     * This method scans all environment variables, filters those that start with the prefix "KAFKA_",
+     * converts them to a property key format, and collects them into a Properties object.
+     *
+     * @return Properties object containing Kafka-related properties derived from environment variables.
+     */
+    public static Properties getKafkaPropertiesFromEnv() {
+        Properties properties = new Properties();
+        properties.putAll(System.getenv()
+                .entrySet()
+                .stream()
+                .filter(mapEntry -> mapEntry.getKey().startsWith(KAFKA_PREFIX))
+                .collect(Collectors.toMap(mapEntry -> convertEnvVarToPropertyKey(mapEntry.getKey()), Map.Entry::getValue)));
+        return properties;
+    }
+}
